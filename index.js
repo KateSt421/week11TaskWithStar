dcc75a5acc8c811f8105dac892c2c2c837a006a6
KateSt421 <belporibet@mail.ru>
2025-01-25 22:14:08 +0300

    Добавила задание с* 11 неделя

---
+++ index.js
@@ -0,0 +1,16 @@
+function calculateTotalPrice() {
+  const prices = document.querySelectorAll('.price p');
+  
+  let totalPrice = 0;
+
+  prices.forEach(priceElement => {
+      const priceText = priceElement.innerHTML; 
+      const priceValue = +priceText; 
+      totalPrice += priceValue; 
+  });
+
+  const resultElement = document.getElementById('result');
+  resultElement.innerHTML = `Итого с 20% скидкой: ${totalPrice * 20 / 100} рублей`;
+}
+
+document.getElementById('calculate').addEventListener('click', calculateTotalPrice);
